ecc4f705cbbf5d904959b0aa28e8a971e25edc10
Mykyta Dotsenko <mykytadotsenko@Mykytas-MacBook-Pro.local>
2023-06-05 18:31:02 +0300

    upd

--- max.py
+++ max.py
@@ -1,10 +1,8 @@
 import random
 
-# Определение функции, которую нужно максимизировать
 def fitness_function(x):
     return 78 + 9*x - 2*x**2 + 3*x**3
 
-# Создание начальной популяции случайных решений
 def create_initial_population(population_size, min_value, max_value):
     population = []
     for _ in range(population_size):
@@ -12,47 +10,38 @@ def create_initial_population(population_size, min_value, max_value):
         population.append(individual)
     return population
 
-# Оценка приспособленности каждого решения в популяции
 def calculate_fitness(population):
     fitness_scores = []
     for individual in population:
         fitness_scores.append(fitness_function(individual))
     return fitness_scores
 
-# Выбор родителей для скрещивания (пропорционально приспособленности)
 def select_parents(population, fitness_scores):
-    # Выбираем двух родителей, используя рулеточный выбор
     parent1 = random.choices(population, weights=fitness_scores)[0]
     parent2 = random.choices(population, weights=fitness_scores)[0]
     return parent1, parent2
 
-# Скрещивание двух родителей для создания потомка
 def crossover(parent1, parent2):
-    # Одноточечное скрещивание
     child = (parent1 + parent2) / 2
     return child
 
-# Мутация потомка
 def mutate(child, mutation_rate):
     if random.random() < mutation_rate:
         child = random.uniform(min_value, max_value)
     return child
 
-# Генетический алгоритм для поиска максимума функции
 def genetic_algorithm(population_size, min_value, max_value, num_generations, mutation_rate):
     population = create_initial_population(population_size, min_value, max_value)
     
     for generation in range(num_generations):
         fitness_scores = calculate_fitness(population)
         
-        # Находим максимальное значение приспособленности
         max_fitness = max(fitness_scores)
         max_index = fitness_scores.index(max_fitness)
         best_individual = population[max_index]
         
-        print(f"Поколение {generation}: Лучшее значение = {best_individual}, Максимальная приспособленность = {max_fitness}")
+        print(f"generation {generation}: best individual = {best_individual}, max fitness = {max_fitness}")
         
-        # Создаем новую популяцию для следующего поколения
         new_population = []
         for _ in range(population_size):
             parent1, parent2 = select_parents(population, fitness_scores)
@@ -64,13 +53,11 @@ def genetic_algorithm(population_size, min_value, max_value, num_generations, mu
     
     return best_individual
 
-# Параметры генетического алгоритма
 population_size = 100
 min_value = -12
 max_value = 51
 num_generations = 50
 mutation_rate = 0.01
 
-# Запуск генетического алгоритма
 best_solution = genetic_algorithm(population_size, min_value, max_value, num_generations, mutation_rate)
-print("Максимум функции:", fitness_function(best_solution))
+print("max:", fitness_function(best_solution))
